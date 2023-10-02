2b03eb7a279ec837558d709e0b82b9a74af45ba4
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2023-10-02 02:29:30 +0000

    update query

--- mylib/query.py
+++ mylib/query.py
@@ -7,7 +7,7 @@ def query():
     """Query the database for the top 1 row of the listingDB table"""
     conn = sqlite3.connect("listingDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT bedrooms FROM listingDB LIMIT 2")
+    cursor.execute("SELECT id, bedrooms, price FROM listingDB LIMIT 10")
     
     print(cursor.fetchall())
     conn.close()

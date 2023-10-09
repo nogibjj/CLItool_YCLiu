7078649edd30539877fab5c12e167482d21f781d
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2023-10-09 20:42:56 +0000

    adjust test cases

--- test_main.py
+++ test_main.py
@@ -1,8 +1,18 @@
-
+from mylib.create import create
+from mylib.query import query
 
 def testMain():
-    pass
-
+    create()
+    output = query()
+    # test GROUPBY
+    assert output[0][3] == 260
+    # test ORDERBY
+    assert output[0][3] > output[1][3]
+    assert output[1][3] > output[2][3]
+    # test WHERE
+    sex_l = [t[2] for t in output]
+    assert 'Male' not in sex_l
+    assert len(output) == 3
 if __name__ == '__main__':
     testMain()
     pass
